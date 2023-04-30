cee7a99789a4c553d2ec549fa293db5388e54778
Milena <milena.velichkova.1729@gmail.com>
2023-04-30 13:09:53 +0300

    Quantity and User classes added

---
+++ CookingRecipes/src/main/java/model/Quantity.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum Quantity {
+    GRAM("гр"),
+    MILLILITRES("мл"),
+    COUNT("бр");
+
+    private final String quantityName;
+
+    Quantity(String name) {
+        this.quantityName = name;
+    }
+
+    public String getQuantityName(){
+        return quantityName;
+    }
+}
---
+++ CookingRecipes/src/main/java/model/User.java
@@ -0,0 +1,10 @@
+package model;
+import java.util.List;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private List<Recipe> favourites;
+}
