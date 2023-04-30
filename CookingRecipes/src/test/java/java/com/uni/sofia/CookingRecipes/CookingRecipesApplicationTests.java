3c85617f3d8c26aafe697d5f87cd32d49bfae52c
VikiIvanova <viki.ivanova756971@gmail.com>
2023-04-30 11:36:47 +0300

    Added empty spring project

---
+++ CookingRecipes/src/test/java/java/com/uni/sofia/CookingRecipes/CookingRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.uni.sofia.CookingRecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
