a3cc1ca5ae76e94e369881c30ab6e2f900b18b38
Milena <milena.velichkova.1729@gmail.com>
2023-05-28 15:21:14 +0300

    Data base creation

---
+++ CookingRecipes/src/main/java/com/example/demo/enums/Measure.java
@@ -0,0 +1,19 @@
+package com.example.demo.enums;
+
+
+public enum Measure {
+    GRAM("гр"),
+    MILLILITRES("мл"),
+    COUNT("бр");
+
+    private final String measureName;
+
+    Measure(String name) {
+        this.measureName = name;
+
+    }
+
+    public String getMeasureName(){
+        return measureName;
+    }
+}
