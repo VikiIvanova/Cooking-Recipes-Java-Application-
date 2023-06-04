246c0eb82c8a5b79f6e38450a4fd2a8885dc8996
Milena <milena.velichkova.1729@gmail.com>
2023-06-04 16:06:30 +0300

    dto for creating users is added

--- CookingRecipes/src/main/java/com/example/demo/model/Product.java
+++ CookingRecipes/src/main/java/com/example/demo/model/Product.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import com.example.demo.enums.Measure;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -8,5 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+    @Column(name = "product_name")
+    private String name;
 
+    @Column(name = "quantity")
+    private Double quantity;
+
+    @Column(name = "measure")
+    private Measure measure;
 }
