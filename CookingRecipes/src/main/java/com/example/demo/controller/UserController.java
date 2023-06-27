668fec647577e43de2b42285a4d5c8b65d4af391
Milena <milena.velichkova.1729@gmail.com>
2023-06-27 14:59:35 +0300

    user authentication implemented

--- CookingRecipes/src/main/java/com/example/demo/controller/UserController.java
+++ CookingRecipes/src/main/java/com/example/demo/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.CreateUserDto;
+import com.example.demo.dto.LoginUserDto;
 import com.example.demo.dto.UpdateUserDto;
+import com.example.demo.dto.UserDto;
 import com.example.demo.mapper.UserMapper;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +28,9 @@ public class UserController {
 
     @PostMapping("/createuser")
     public Long createUser(@RequestBody CreateUserDto userDto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(14);
+        String hashedPassword = encoder.encode(userDto.getPassword());
+        userDto.setPassword(hashedPassword);
         return userService.createUser(userMapper.toEntity(userDto));
     }
 
@@ -37,10 +43,26 @@ public class UserController {
                 .collect(Collectors.toList());
     }
 
+    @GetMapping("/user/{id}")
+    public UserDto getUserByID(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return userMapper.toUserDto(user);
+    }
+
     @PutMapping("/update/{id}")
-    public User updateUser (@PathVariable Long id, @RequestBody @Valid UpdateUserDto userDto){
-        userService.updateUser(id,userMapper.toEntity(userDto));
+    public User updateUser(@PathVariable Long id, @RequestBody @Valid UpdateUserDto userDto) {
+        userService.updateUser(id, userMapper.toEntity(userDto));
         return userMapper.toEntity(userDto);
     }
 
+    @PostMapping("/login")
+    public Long login(@Valid @RequestBody LoginUserDto loginUserDto) {
+        Long toReturnId = -1L;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(14);
+        User user = userService.findUserByEmail(loginUserDto.getEmail());
+        if (encoder.matches(loginUserDto.getPassword(), user.getPassword())) {
+            toReturnId = user.getId();
+        }
+        return toReturnId;
+    }
 }
