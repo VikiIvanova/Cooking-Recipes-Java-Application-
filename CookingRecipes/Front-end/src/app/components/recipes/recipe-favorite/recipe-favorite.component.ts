0fc247d7bcb8359f9a9a0b78556712b714485af1
VikiIvanova <viki.ivanova756971@gmail.com>
2023-06-30 14:08:49 +0300

    Fixed showing image in favorite recipes

--- CookingRecipes/Front-end/src/app/components/recipes/recipe-favorite/recipe-favorite.component.ts
+++ CookingRecipes/Front-end/src/app/components/recipes/recipe-favorite/recipe-favorite.component.ts
@@ -1,6 +1,48 @@
-import { Component } from '@angular/core';
-import { RecipeModel } from '../../../interfaces/recipe.model';
-import { RecipeService } from '../../../services/recipe.service';
+// import { Component } from '@angular/core';
+// import { RecipeModel } from '../../../interfaces/recipe.model';
+// import { RecipeService } from '../../../services/recipe.service';
+//
+// @Component({
+//   selector: 'app-recipe-favorite',
+//   templateUrl: './recipe-favorite.component.html',
+//   styleUrls: ['./recipe-favorite.component.css']
+// })
+// export class RecipeFavoriteComponent {
+//   favoriteRecipes: RecipeModel[] = [];
+//   public userId!: number;
+//
+//   constructor(private recipeService: RecipeService) { }
+//
+//   ngOnInit(): void {
+//     this.fetchFavoriteRecipes();
+//   }
+//
+//   fetchFavoriteRecipes(): void {
+//     const userId = Number(localStorage.getItem('id'));
+//     this.recipeService.getAllFavoriteRecipes(userId)
+//       .subscribe(recipeIds => {
+//         recipeIds.forEach(recipeId => {
+//           this.recipeService.getRecipeById(recipeId)
+//             .subscribe(recipe => {
+//               this.favoriteRecipes.push(recipe);
+//             });
+//         });
+//       });
+//   }
+//
+//   fetchRecipeImages(): void {
+//     for (const recipe of this.favoriteRecipes) {
+//       this.recipeService.getImagePathByRecipeId(recipe.id)
+//         .subscribe((imageBlob: Blob) => {
+//           recipe.imagePath = URL.createObjectURL(imageBlob);
+//         });
+//     }
+//   }
+// }
+
+import {Component} from '@angular/core';
+import {RecipeModel} from '../../../interfaces/recipe.model';
+import {RecipeService} from '../../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-favorite',
@@ -11,7 +53,8 @@ export class RecipeFavoriteComponent {
   favoriteRecipes: RecipeModel[] = [];
   public userId!: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService) {
+  }
 
   ngOnInit(): void {
     this.fetchFavoriteRecipes();
@@ -25,8 +68,19 @@ export class RecipeFavoriteComponent {
           this.recipeService.getRecipeById(recipeId)
             .subscribe(recipe => {
               this.favoriteRecipes.push(recipe);
+              this.fetchRecipeImages();
             });
         });
       });
   }
+
+  fetchRecipeImages(): void {
+    for (const recipe of this.favoriteRecipes) {
+      this.recipeService.getImagePathByRecipeId(recipe.id)
+        .subscribe((imageBlob: Blob) => {
+          recipe.imagePath = URL.createObjectURL(imageBlob);
+        });
+    }
+  }
 }
+
